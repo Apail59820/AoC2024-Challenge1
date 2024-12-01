1baf770c226496254e9203c1a7b7a614024b577a
Amaury Paillart <amaury.paillart@groupe-projex.fr>
2024-12-01 20:33:33 +0100

    o(n^2) into o(1)

--- main.cpp
+++ main.cpp
@@ -1,10 +1,8 @@
-//
-// Created by Amaury Paillard on 01/12/2024.
-//
-
 #include <fstream>
 #include <iostream>
-
+#include <unordered_map>
+#include <vector>
+#include <chrono>
 
 bool LoadListFromFile(std::vector<std::pair<int, int>>& vec) {
     std::ifstream inputFile("../day1Input.txt");
@@ -26,43 +24,35 @@ int main() {
     std::vector<std::pair<int, int>> lists;
     const auto start = std::chrono::high_resolution_clock::now();
 
-    if(!LoadListFromFile(lists)) {
+    if (!LoadListFromFile(lists)) {
         std::cerr << "Error while loading lists." << std::endl;
+        return 1;
     }
 
-    std::vector<int> leftList, rightList;
-    for (const auto&[fst, snd] : lists) {
-        leftList.push_back(fst);
-        rightList.push_back(snd);
+    std::unordered_map<int, int> leftFreq, rightFreq;
+
+    for (const auto& [left, right] : lists) {
+        ++leftFreq[left];
+        ++rightFreq[right];
     }
-
-    std::sort(leftList.begin(), leftList.end());
-    std::sort(rightList.begin(), rightList.end());
-
-
-    lists.clear();
 
     int totalDist = 0;
     int similarityScore = 0;
 
-    if(leftList.size() == rightList.size()) {
-        const int listSize = static_cast<int>(leftList.size());
-        int count = 0;
-
-        for(int i = 0; i < listSize; ++i) {
-
-            count = static_cast<int>(std::count(rightList.begin(), rightList.end(), leftList[i]));
-            similarityScore += leftList[i] * count;
-
-            totalDist += rightList[i] - leftList[i];
+    for (const auto& [leftValue, leftCount] : leftFreq) {
+        if (rightFreq.find(leftValue) != rightFreq.end()) {
+            similarityScore += leftValue * leftCount * rightFreq[leftValue];
         }
+    }
+
+    for (const auto& [left, right] : lists) {
+        totalDist += right - left;
     }
 
     std::cout << "Total distance : " << totalDist << std::endl;
     std::cout << "Similarity score : " << similarityScore << std::endl;
 
     const auto end = std::chrono::high_resolution_clock::now();
-
     const std::chrono::duration<double, std::milli> elapsed = end - start;
     std::cout << "Finished in: " << elapsed.count() << " ms" << std::endl;
 
